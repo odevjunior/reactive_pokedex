1f76290925182a88430affe40cad0688ab2597ff
Jairo Junior <jairo.ajunior@mercadolivre.com>
2021-09-27 14:54:17 -0300

    initial scaffolding

---
+++ src/test/java/com/odevjunior/pokedex/PokedexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odevjunior.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
